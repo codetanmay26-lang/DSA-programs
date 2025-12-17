4d2e604162ca83c4322b93f43a2036f53a566767
TANMAY SHARMA <codetanmay26@gmail.com>
2025-12-18 01:30:32 +0530

    Time: 0 ms (100%), Space: 8.5 MB (91.86%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // negative numbers or numbers ending with 0 (except 0 itself)
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int rev = 0;
+
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even digits: x == rev
+        // For odd digits: x == rev/10
+        return (x == rev || x == rev / 10);
+    }
+};
