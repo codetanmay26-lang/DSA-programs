f3d3cecd0065fed9acbd7cbf21732fe004b406fe
TANMAY SHARMA <codetanmay26@gmail.com>
2026-04-13 01:45:49 +0530

    Time: 0 ms (100%), Space: 12.7 MB (39.25%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -7,13 +7,13 @@ public:
         int low=0;
         while(low<high){
             int mid = (low+high)/2;
-            if(nums[mid]<nums[mid+1]){
+            if(nums[mid]<nums[mid+1]){// increasing slope → peak on right
                 low=mid+1;
             }
-            else{
+            else{// decreasing slope → peak on left (including mid)
                 high=mid;
             }
     }
-    return low;
+    return low;// index of peak
     }
 };
